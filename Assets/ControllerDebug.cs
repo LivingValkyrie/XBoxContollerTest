3a071b07d035cfc4e036d25881cc52317ff47250
LivingValkyrie <deadwynn@gmail.com>
2015-12-13 19:53:18 -0600

    images linked. all buttons scripted - axises remain
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

--- Assets/ControllerDebug.cs
+++ Assets/ControllerDebug.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 /// <summary>
@@ -11,12 +12,33 @@ using System.Collections;
 public class ControllerDebug : MonoBehaviour {
     #region Fields
 
+    //22 images in total
+    public Image aButton, bButton, xButton, yButton;
+    public Image startButton, backButton;
+    public Image rightBumper, leftBumper, rightTrigger, leftTrigger;
+    public Image lsUp, lsDown, lsRight, lsLeft;
+    public Image rsUp, rsDown, rsRight, rsLeft;
+    public Image dpadUp, dpadDown, dpadRight, dpadLeft;
+
     #endregion
 
     void Start() { }
 
     void Update() {
-        print(Input.GetAxis("LeftJoystick_Horizontal"));
+        aButton.color = (Input.GetButton("Button_A")) ? Color.red : Color.white;
+        bButton.color = ( Input.GetButton( "Button_B" ) ) ? Color.red : Color.white;
+        xButton.color = ( Input.GetButton( "Button_X" ) ) ? Color.red : Color.white;
+        yButton.color = ( Input.GetButton( "Button_Y" ) ) ? Color.red : Color.white;
+        startButton.color = ( Input.GetButton( "Button_Start" ) ) ? Color.red : Color.white;
+        backButton.color = ( Input.GetButton( "Button_Back" ) ) ? Color.red : Color.white;
+        rightBumper.color = ( Input.GetButton( "Bumper_Right" ) ) ? Color.red : Color.white;
+        leftBumper.color = ( Input.GetButton( "Bumper_Left" ) ) ? Color.red : Color.white;
+    }
+
+    Color CalculateColor(float input) {
+        
+
+        return Color.black;
     }
 
 }
