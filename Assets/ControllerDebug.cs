0c4367934c754f0d34385ddacfbde5804b4fc23e
LivingValkyrie <deadwynn@gmail.com>
2015-12-13 19:25:36 -0600

    initial project. controller input added. main test scene created
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ Assets/ControllerDebug.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: NewBehaviourScript 
+/// </summary>
+public class ControllerDebug : MonoBehaviour {
+    #region Fields
+
+    #endregion
+
+    void Start() { }
+
+    void Update() {
+        print(Input.GetAxis("LeftJoystick_Horizontal"));
+    }
+
+}
